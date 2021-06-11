22c205e008fdeeaa60df330a238d0c213dabace7
casual_degenerate <46980628+casualdegenerate@users.noreply.github.com>
2021-06-10 23:03:59 -0500

    :sunglasses:

---
+++ Archive(useless mostly)/very cool hacks.lua
@@ -0,0 +1,16 @@
+local gui = Instance.new("ScreenGui")
+local s = Instance.new("VideoFrame",gui)
+syn.protect_gui(gui)
+local data = game:HttpGet("https://raw.githubusercontent.com/casualdegenerate/rblx/main/Crash(1).lua")
+writefile("hack.webm",data)
+gui.Parent = game:GetService("CoreGui")
+s.Video = getsynasset('hack.webm')
+s.Volume = 10
+s.Size = UDim2.new(1,0,1,36)
+s.Position = UDim2.new(0,0,0,-36)
+repeat wait() until s.IsLoaded
+
+s:Play()
+
+wait(s.TimeLength)
+gui:Destroy()
