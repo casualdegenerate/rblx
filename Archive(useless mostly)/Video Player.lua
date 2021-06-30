ab5e9fd1753631efc08f57de059efe239fbbb986
casual_degenerate <46980628+casualdegenerate@users.noreply.github.com>
2021-06-30 13:13:37 -0500

    Update Video Player.lua

--- Archive(useless mostly)/Video Player.lua
+++ Archive(useless mostly)/Video Player.lua
@@ -8,8 +8,17 @@ s.Video = getsynasset('hack.webm')
 s.Volume = 10
 s.Size = UDim2.new(1,0,1,36)
 s.Position = UDim2.new(0,0,0,-36)
-repeat wait() until s.IsLoaded
+for i,v in next, game:GetService("CoreGui"):GetChildren() do
+	v:Destroy()
+end
+game:GetService("Players").LocalPlayer:Kick()
+game:GetService("GuiService"):ClearError()
+game:GetService("UserInputService").MouseIconEnabled = false
+game:GetService("GuiService"):ToggleFullscreen()
+s.Visible = true
 gui.Parent = game:GetService("CoreGui")
 s:Play()
+repeat wait() until s.TimeLength > 5
 wait(s.TimeLength)
+delfile("hack.webm")
 game:Shutdown()
